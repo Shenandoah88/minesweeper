776d7bf1346f6e40d7ffb47369c16177717445e6
Farris Skaff <farrisskaff@Farriss-MacBook-Pro.local>
2019-05-29 22:03:48 -0600

    Minesweeper game files created

---
+++ game.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Minesweeper Game</title>
+        <link href="style.css" rel="stylesheet" type="text/css" media="all">
+    </head>
+    <body>
+
+        <script src="logic.js"></script>
+    </body>
+</html>
